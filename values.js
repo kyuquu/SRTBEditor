a43f7d3f94fd543337b577a6fc462346523c76f0
kyuquu <105995408+kyuquu@users.noreply.github.com>
2024-12-05 22:24:55 -0500

    Create framework for basic view
    Also update version number

--- values.js
+++ values.js
@@ -10,12 +10,25 @@ const TBChartFilename = document.getElementById("tb-chart-filename");
 
 const JSONEditor = document.getElementById("json-editor");
 
-function getJSONValue(referenceArray) {
+function getJSONValue(defaultValue, referenceArray) {
     let JSONValue = chartJSON;
-    for (let i = 0; i < referenceArray.length; i++) {
-       JSONValue = JSONValue[referenceArray[i]];
+    referenceLoop: for (let i = 0; i < referenceArray.length; i++) {
+        if (JSONValue[referenceArray[i]]) {
+            JSONValue = JSONValue[referenceArray[i]];
+        }
+        else {
+            JSONValue = defaultValue;
+            break referenceLoop;
+        }
     }
     return JSONValue;
+}
+
+function updateValue(valueName, value) {
+    if (document.getElementById(`bv-${valueName}`)) {
+        let BVElement = document.getElementById(`bv-${valueName}`);
+        BVElement.value = value;
+    }
 }
 
 function loadChartData(data) {
@@ -30,7 +43,8 @@ function loadChartData(data) {
 
         let trackInfoKeys = Object.keys(trackInfo);
         for (let i = 0; i < trackInfoKeys.length; i++) {
-            trackInfo[trackInfoKeys[i]]["value"] = getJSONValue(trackInfo[trackInfoKeys[i]]["reference"]);
+            trackInfo[trackInfoKeys[i]]["value"] = getJSONValue(trackInfo[trackInfoKeys[i]]["default"], trackInfo[trackInfoKeys[i]]["reference"]);
+            updateValue(trackInfoKeys[i], trackInfo[trackInfoKeys[i]]["value"]);
         }
     
         TBChartTitle.textContent = trackInfo["title"]["value"];
