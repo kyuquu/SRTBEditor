01c74d43cde4b4823c4e89f5d6325588a01fe210
kyuquu <105995408+kyuquu@users.noreply.github.com>
2024-12-19 17:11:54 -0500

    Begin implementing .zip download functionality
    Also change the way libraries are loaded

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+let JSONEditor = ace.edit("editor");
+JSONEditor.setTheme("ace/theme/dracula");
+JSONEditor.session.setMode("ace/mode/json");
+JSONEditor.setOptions({
+    fixedWidthGutter: true,
+    showPrintMargin: false
+});
+
+
+
 function switchToTab(index) {
     if (document.getElementById(`tab-button${index}`).classList.contains("tab-button-inactive")) {
         document.getElementById(`tab-button${index}`).classList.remove("tab-button-inactive");
@@ -24,27 +34,32 @@ function saveAsJSON() {
     downloadFile(filename, json);
 }
 
-function saveAsZIP() {
-    // write this function later once libraries are working
+function saveAsZIP() { // this function needs to be edited once album art and audio is supported
+    let filename = chartFilename.split(".").slice(0, -1).join(".");
+    let srtb = JSON.stringify(convertToSRTB(JSON.parse(JSONEditor.getValue())));
+
+    let zip = new JSZip();
+    zip.file(`${filename}.srtb`, srtb);
+    zip.generateAsync({ type: "blob" }).then((content) => {
+        saveAs(content, `BACKUP_${filename}.zip`);
+    });
 }
+
+
 
 function convertToJSON(srtb) {
     let data = srtb["largeStringValuesContainer"]["values"];
-
     for (let i = 0; i < data.length; i++) {
         data[i]["val"] = JSON.parse(data[i]["val"]);
     }
-
     return srtb;
 }
 
 function convertToSRTB(json) {
     let data = json["largeStringValuesContainer"]["values"];
-
     for (let i = 0; i < data.length; i++) {
         data[i]["val"] = JSON.stringify(data[i]["val"]);
     }
-
     return json;
 }
 
@@ -52,7 +67,7 @@ function convertToSRTB(json) {
 
 function downloadFile(filename, file) {
     let link = document.createElement("a"); 
-    link.setAttribute("href", "data:text/plain; charset=utf-8," + encodeURIComponent(file));
+    link.setAttribute("href", `data:text/plain; charset=utf-8, ${encodeURIComponent(file)}`);
     link.setAttribute("download", filename);
     link.style.display = "none";
     document.body.appendChild(link);
