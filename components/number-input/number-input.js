430b28f9ba2c0b7b0362777e989411113126132f
GaviGuy <gaviguy711@gmail.com>
2025-05-29 22:14:57 -0600

    adjustments for sizing

--- components/number-input/number-input.js
+++ components/number-input/number-input.js
@@ -7,7 +7,7 @@ function initializeNumberInput(name, values) {
     element.removeAttribute("id");
     element.innerHTML = `
         <label for="${name}">${text}</label>
-        <input type="number" min="1" step="1" onkeypress="return event.keyCode === 8 || event.charCode >= 48 && event.charCode <= 57"
+        <input type="number" step="1" onkeypress="return event.keyCode === 8 || event.charCode >= 48 && event.charCode <= 57"
                 id="${name}" onblur="this.scrollLeft=0" onchange="${func}">
     `;
 }
