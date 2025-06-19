ca2e1330302210e599111c05ddd03dafbc2bb606
GaviGuy <gaviguy711@gmail.com>
2025-06-19 13:17:13 -0600

    number input compatibility and negatives

--- components/number-input/number-input.js
+++ components/number-input/number-input.js
@@ -7,7 +7,7 @@ function initializeNumberInput(name, values) {
     element.removeAttribute("id");
     element.innerHTML = `
         <label for="${name}">${text}</label>
-        <input type="number" step="1" onkeypress="return event.keyCode === 8 || event.charCode >= 48 && event.charCode <= 57"
+        <input type="number" step="1" onkeypress="return event.key === '-' || event.key === '.' || event.key === ',' || event.key >= '0' && event.key <= '9'"
                 id="${name}" onblur="this.scrollLeft=0" onchange="${func}">
     `;
 }
