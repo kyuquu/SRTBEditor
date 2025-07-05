d843c35c17d3b065ab0f95f9466be358068c86b6
GaviGuy <gaviguy711@gmail.com>
2025-07-05 02:19:06 -0600

    add prompt when tabbing without saving

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,18 @@ let activeDifficulty = 2;
 
 function switchToTab(index) {
     if (activeTab !== index) {
+        
+        // prompt if unsaved changes in the JSON editor
+        if (activeTab === 1
+                && !(document.getElementById("json-editor").classList.contains("disabled"))
+                && !(document.getElementById("jv-discard").classList.contains("disabled"))) {
+            if(confirm("Leave and discard JSON Editor changes?")) {
+                discardEditorChanges();
+            } else {
+                return;
+            }
+        }
+
         document.getElementById(`tab-button${activeTab}`).classList.remove("active");
         document.getElementById(`tab${activeTab}`).classList.remove("active");
         document.getElementById(`tab-button${index}`).classList.add("active");
