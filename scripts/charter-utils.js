18dd6da1fdbe9004e43f4752f783047c13b55619
GaviGuy <gaviguy711@gmail.com>
2025-07-09 22:11:40 -0600

    console spam cleanup

--- scripts/charter-utils.js
+++ scripts/charter-utils.js
@@ -22,7 +22,6 @@ function stackNearbyNotes(noteData) {
         if(noteData[i].time - prevTime < 0.0005 && noteData[i].time !== prevTime) {
             noteData[i].time = prevTime;
             numChanges++;
-            console.log(`adjusted a note at t=${noteData[i].time}`);
         }
         else {
             prevTime = noteData[i].time;
