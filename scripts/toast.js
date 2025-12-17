eb778acf649e70e97ec68dd953c8c62cf420309f
GaviGuy <gaviguy711@gmail.com>
2025-12-17 13:57:35 -0700

    functional toasts

---
+++ scripts/toast.js
@@ -0,0 +1,34 @@
+let toastId = 0;
+
+function createToast (title, text, type, timeout, action) {
+    let id = toastId;
+    toastId++;
+    let toast = document.createElement("div");
+    toast.classList.add("toast");
+    toast.classList.add(type);
+    toast.setAttribute("id", `toast-${id}`);
+    if(action)
+        toast.setAttribute("onclick", () => {removeToast(id)});
+    else 
+        toast.setAttribute("onclick", `removeToast(${id})`);
+
+    let toastTitle = document.createElement("div");
+    toastTitle.textContent = title;
+    toastTitle.classList.add("toast-title");
+    toast.appendChild(toastTitle);
+    let toastContent = document.createElement("div");
+    toastContent.textContent = text;
+    toastContent.classList.add("toast-content");
+    toast.appendChild(toastContent);
+
+    document.getElementById("toast-container").appendChild(toast);
+
+    if(timeout && timeout > 0)
+        setTimeout(removeToast, timeout, id);
+}
+
+function removeToast(id) {
+    let elem = document.getElementById(`toast-${id}`);
+    console.log("removed toast " + id);
+    elem.remove();
+}
